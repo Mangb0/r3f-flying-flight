572b2eb8ceb8b86d56a39eabb927d5f101771e45
Mangb0 <whdudghks45629@gmail.com>
2024-01-20 23:20:51 +0900

    모델 추가(Airplane, Cloud) 및 랜덤 배치 구현

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,15 +1,49 @@
-import { OrbitControls } from "@react-three/drei";
+import { Float, OrbitControls } from "@react-three/drei";
 import { Background } from "./Background";
+import { Airplane } from "./Airplane";
+import { Cloud } from "./Cloud";
+
+let cloudArray = new Array(10);
 
 export const Experience = () => {
+  const cloudRender = () => {
+    const result = [];
+    const opacity = Math.random() * 0.5 + 0.3;
+    const rotation = Math.random() > 0.5 ? Math.PI / 9 : 0;
+
+    for (let i = 0; i < 10; i++) {
+      result.push(
+        <Cloud
+          opacity={opacity}
+          scale={[
+            Math.random() * 0.5 + 0.3,
+            Math.random() * 0.5 + 0.3,
+            Math.random() * 0.5 + 0.3,
+          ]}
+          position={[
+            Math.random() * 4 - 2,
+            Math.random() * 4 - 2,
+            Math.random() * 100 - 100,
+          ]}
+          rotation-y={rotation}
+        />
+      );
+    }
+    return result;
+  };
+
   return (
     <>
       <OrbitControls />
       <Background />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Float floatIntensity={2} speed={2}>
+        <Airplane
+          rotation-y={Math.PI / 2}
+          scale={[0.2, 0.2, 0.2]}
+          position-y={0.1}
+        />
+      </Float>
+      {cloudRender()}
     </>
   );
 };
