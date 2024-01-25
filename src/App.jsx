d28c8b794e000a420cc5048d35dcc5cfeab06f3c
Mangbo <whdudghks45629@gmail.com>
2024-01-25 23:29:31 +0900

    스크롤 동작 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { ScrollControls } from "@react-three/drei";
 
 const App = () => {
   return (
     <Canvas>
       <color attach="background" args={["#ececec"]} />
-      <Experience />
+      <ScrollControls pages={5} damping={0.3}>
+        <Experience />
+      </ScrollControls>
     </Canvas>
   );
 };
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,16 @@
-import { Float, Line, OrbitControls } from "@react-three/drei";
+import {
+  Float,
+  Line,
+  OrbitControls,
+  PerspectiveCamera,
+  useScroll,
+} from "@react-three/drei";
 import { Background } from "./Background";
 import { Airplane } from "./Airplane";
 import { Cloud } from "./Cloud";
-import { useMemo } from "react";
+import { useMemo, useRef } from "react";
 import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 
 export const Experience = () => {
   const cloudRender = () => {
@@ -32,7 +39,7 @@ export const Experience = () => {
     return result;
   };
 
-  const LINE_NB_POINTS = 2000;
+  const LINE_NB_POINTS = 12000;
 
   const curve = useMemo(() => {
     return new THREE.CatmullRomCurve3(
@@ -67,17 +74,64 @@ export const Experience = () => {
     return shape;
   }, [curve]);
 
+  const cameraGroup = useRef();
+  const scroll = useScroll();
+
+  const airplane = useRef();
+
+  useFrame((_state, delta) => {
+    const curPointIndex = Math.min(
+      Math.round(scroll.offset * linePoints.length),
+      linePoints.length - 1
+    );
+    const curPoint = linePoints[curPointIndex];
+    const pointAhead =
+      linePoints[Math.min(curPointIndex + 1, linePoints.length - 1)];
+
+    const xDisplacement = (pointAhead.x - curPoint.x) * 80;
+
+    const angleRotation =
+      (xDisplacement < 0 ? 1 : -1) *
+      Math.min(Math.abs(xDisplacement), Math.PI / 3);
+
+    const targetAirplaneQuaternion = new THREE.Quaternion().setFromEuler(
+      new THREE.Euler(
+        airplane.current.rotation.x,
+        airplane.current.rotation.y,
+        angleRotation
+      )
+    );
+
+    const targetCameraQuaternion = new THREE.Quaternion().setFromEuler(
+      new THREE.Euler(
+        airplane.current.rotation.x,
+        angleRotation,
+        airplane.current.rotation.z
+      )
+    );
+
+    airplane.current.quaternion.slerp(targetAirplaneQuaternion, delta * 2);
+    cameraGroup.current.quaternion.slerp(targetCameraQuaternion, delta * 2);
+
+    cameraGroup.current.position.lerp(curPoint, delta * 24);
+  });
+
   return (
     <>
-      <OrbitControls />
-      <Background />
-      <Float floatIntensity={2} speed={2}>
-        <Airplane
-          rotation-y={Math.PI / 2}
-          scale={[0.3, 0.3, 0.3]}
-          position-y={0.1}
-        />
-      </Float>
+      {/* <OrbitControls enableZoom={false} /> */}
+      <group ref={cameraGroup}>
+        <Background />
+        <PerspectiveCamera position={[0, 0, 5]} fov={30} makeDefault />
+        <group ref={airplane}>
+          <Float floatIntensity={2} speed={2}>
+            <Airplane
+              rotation-y={Math.PI / 2}
+              scale={[0.2, 0.2, 0.2]}
+              position-y={0.1}
+            />
+          </Float>
+        </group>
+      </group>
 
       <group position-y={-2}>
         {/* <Line
