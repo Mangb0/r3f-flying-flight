5a6d4066eb726383280914fe18330a034ace91e3
Mangbo <whdudghks45629@gmail.com>
2024-02-06 22:25:51 +0900

    Noise 효과 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { ScrollControls } from "@react-three/drei";
+import { EffectComposer, Noise } from "@react-three/postprocessing";
 
 const App = () => {
   return (
@@ -9,6 +10,9 @@ const App = () => {
       <ScrollControls pages={20} damping={0.5}>
         <Experience />
       </ScrollControls>
+      <EffectComposer>
+        <Noise opacity={0.3} />
+      </EffectComposer>
     </Canvas>
   );
 };
