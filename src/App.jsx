612e8551967e48b9f1d0eab02607b87120c46e0b
Mangbo <whdudghks45629@gmail.com>
2024-02-09 01:18:43 +0900

    Overlay 화면 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import { ScrollControls } from "@react-three/drei";
 import { EffectComposer, Noise } from "@react-three/postprocessing";
+import { Overlay } from "./components/Overlay";
 
 const App = () => {
   return (
-    <Canvas>
-      <color attach="background" args={["#ececec"]} />
-      <ScrollControls pages={20} damping={0.5}>
-        <Experience />
-      </ScrollControls>
-      <EffectComposer>
-        <Noise opacity={0.3} />
-      </EffectComposer>
-    </Canvas>
+    <>
+      <Canvas>
+        <color attach="background" args={["#ececec"]} />
+        <ScrollControls pages={20} damping={0.5}>
+          <Experience />
+        </ScrollControls>
+        <EffectComposer>
+          <Noise opacity={0.2} />
+        </EffectComposer>
+      </Canvas>
+      <Overlay />
+    </>
   );
 };
 
---
+++ src/components/Overlay.jsx
@@ -0,0 +1,10 @@
+export const Overlay = () => {
+  return (
+    <div className="overlay">
+      <div className="intro">
+        <h1 className="logo">FLIGHT</h1>
+        <button className="explore">Explore</button>
+      </div>
+    </div>
+  );
+};
